c26554d1b881fe75b1caa90c21145efbfe0046a6
Ammar-Man <almandaa@arcada.fi>
2022-11-22 23:46:29 +0200

    code fix

--- Assets/ballen.cs
+++ Assets/ballen.cs
@@ -15,21 +15,10 @@ public class ballen : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        // lektion 1.11.2021 (tre)
-
-        //float y = Mathf.Sin(Mathf.Deg2Rad.* Time.time);
-        
         y = A * Mathf.Sin(2 * Mathf.PI * (1 / T) * Time.time);
-        //y += Mathf.Sin(Time.time);
-        // transform.position = new Vector3(transform.position.x, y, transform.position.z);
-
-        // float y = A * Mathf.Sin(2 * Mathf.PI * (1 / T) * Time.time);
-        // float x = A * Mathf.Cos(2 * Mathf.PI * (1 / T) * Time.time);
-        // float z = A * Mathf.Sin(2 * Mathf.PI * (1 / T) * Time.time);
-        //transform.position = new Vector3(0, 0, y);
         transform.Rotate(0, 0, y);
 
     }
